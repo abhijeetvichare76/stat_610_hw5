d5b63814c0406bf31f74e26e5baae1fbe115fcd5
abhijeetvichare76 <abhijeetvichare76@gmail.com>
2022-10-17 12:22:24 -0400

    incomplete save

--- llr_functions.R
+++ llr_functions.R
@@ -1,17 +1,17 @@
 llr = function(x,y,z,omega){
   fits = sapply(z,compute_f_hat,x,y,omega)
   return(fits)
-}
-
-compute_f_hat = function(z,x,y,omega){
+}compute_f_hat = function(z,x,y,omega){
   Wz = make_weight_matrix(z,x,omega)
   X = make_predictor_matrix(x)
+  #f_hat = c(1,z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
   f_hat = c(1,z) %*% solve(t(X) %*% Wz %*% X) %*% t(X) %*% Wz %*% y
   return (f_hat)
 }
 
 make_weight_matrix = function(z,x,omega){
-  Wz = diag(abs(x -z) /omega)
+#  Wz = diag(abs(x -z) /omega)
+  Wz = c(abs(x-z)/omega)
   return (Wz)
 }
 
@@ -20,3 +20,4 @@ make_predictor_matrix = function(x){
   X = cbind(ones,x)
   return (X)
 }
+
